ba72981d0d19bb210b04f7ae6405422b55a1749a
Hoxca <hugh@atosc.org>
2021-05-19 23:27:46 +0200

    preserve RGB time count when we got a bunch of short exposures

--- main.go
+++ main.go
@@ -18,21 +18,27 @@ var i = true
 var rootp string
 
 type target struct {
-	name  string
-	L     int
-	Lexpo int
-	R     int
-	Rexpo int
-	G     int
-	Gexpo int
-	B     int
-	Bexpo int
-	S     int
-	Sexpo int
-	H     int
-	Hexpo int
-	O     int
-	Oexpo int
+	name   string
+	L      int
+	Lexpo  int
+	R      int
+	Rexpo  int
+	G      int
+	Gexpo  int
+	B      int
+	Bexpo  int
+	R1     int
+	R1expo int
+	G1     int
+	G1expo int
+	B1     int
+	B1expo int
+	S      int
+	Sexpo  int
+	H      int
+	Hexpo  int
+	O      int
+	Oexpo  int
 }
 
 type targets map[string]target
@@ -117,8 +123,8 @@ func (ts targets) exist(key string) bool {
 }
 
 func (t *target) printObject() {
-	fmt.Printf("filters: %d %d %d %d %d %d %d\n", t.L, t.R, t.G, t.B, t.S, t.H, t.O)
-	fmt.Printf("expt. %d %d %d %d %d %d %d\n", t.Lexpo, t.Rexpo, t.Gexpo, t.Bexpo, t.Sexpo, t.Hexpo, t.Oexpo)
+	fmt.Printf("filters: %d %d %d %d %d %d %d\n", t.L, t.R, t.G, t.B, t.R1, t.G1, t.B1, t.S, t.H, t.O)
+	fmt.Printf("expt. %d %d %d %d %d %d %d\n", t.Lexpo, t.Rexpo, t.Gexpo, t.Bexpo, t.R1expo, t.G1expo, t.B1expo, t.Sexpo, t.Hexpo, t.Oexpo)
 }
 
 func (t *target) iterateFilter(filter string, expo int) {
@@ -127,14 +133,29 @@ func (t *target) iterateFilter(filter string, expo int) {
 		t.L++
 		t.Lexpo = expo
 	case "R":
-		t.R++
-		t.Rexpo = expo
+		if expo > 240 {
+			t.R++
+			t.Rexpo = expo
+		} else {
+			t.R1++
+			t.R1expo = expo
+		}
 	case "G":
-		t.G++
-		t.Gexpo = expo
+		if expo > 240 {
+			t.G++
+			t.Gexpo = expo
+		} else {
+			t.G1++
+			t.G1expo = expo
+		}
 	case "B":
-		t.B++
-		t.Bexpo = expo
+		if expo > 240 {
+			t.B++
+			t.Bexpo = expo
+		} else {
+			t.B1++
+			t.B1expo = expo
+		}
 	case "S":
 		t.S++
 		t.Sexpo = expo
@@ -154,6 +175,9 @@ func newTarget(object string, expo int) *target {
 		R:    0,
 		G:    0,
 		B:    0,
+		R1:   0,
+		G1:   0,
+		B1:   0,
 		S:    0,
 		H:    0,
 		O:    0,
@@ -176,7 +200,11 @@ func (ts *targets) printObjects(wdest writeDestination) {
 	if wdest.writeToConsole {
 		fmt.Printf("Targets list: %q\n\n", targets)
 		for _, v := range *ts {
+			fmt.Println()
 			fmt.Printf("Object: %-30s %s%s\n", v.name, "Total: ", secondsToHuman(v.L*v.Lexpo+v.R*v.Rexpo+v.G*v.Gexpo+v.B*v.Bexpo+v.S*v.Sexpo+v.H*v.Hexpo+v.O*v.Oexpo))
+			if v.L > 0 || v.R > 0 || v.G > 0 || v.B > 0 {
+				fmt.Println()
+			}
 			if v.L > 0 {
 				fmt.Printf("L\tNb: %4d\tExpo: %4ds\tSubs: %s\n", v.L, v.Lexpo, secondsToHuman(v.L*v.Lexpo))
 			}
@@ -188,6 +216,21 @@ func (ts *targets) printObjects(wdest writeDestination) {
 			}
 			if v.B > 0 {
 				fmt.Printf("B\tNb: %4d\tExpo: %4ds\tSubs: %s\n", v.B, v.Bexpo, secondsToHuman(v.B*v.Bexpo))
+			}
+			if v.R1 > 0 || v.G1 > 0 || v.B1 > 0 {
+				fmt.Println()
+			}
+			if v.R1 > 0 {
+				fmt.Printf("R\tNb: %4d\tExpo: %4ds\tSubs: %s\n", v.R1, v.R1expo, secondsToHuman(v.R1*v.R1expo))
+			}
+			if v.G1 > 0 {
+				fmt.Printf("G\tNb: %4d\tExpo: %4ds\tSubs: %s\n", v.G1, v.G1expo, secondsToHuman(v.G1*v.G1expo))
+			}
+			if v.B1 > 0 {
+				fmt.Printf("B\tNb: %4d\tExpo: %4ds\tSubs: %s\n", v.B1, v.B1expo, secondsToHuman(v.B1*v.B1expo))
+			}
+			if v.S > 0 || v.H > 0 || v.O > 0 {
+				fmt.Println()
 			}
 			if v.S > 0 {
 				fmt.Printf("S\tNb: %4d\tExpo: %4ds\tSubs: %s\n", v.S, v.Sexpo, secondsToHuman(v.S*v.Sexpo))
@@ -214,7 +257,11 @@ func (ts *targets) printObjects(wdest writeDestination) {
 
 		fmt.Fprintf(buff, "Targets list: %q\n\n", targets)
 		for _, v := range *ts {
+			fmt.Fprintln(buff)
 			fmt.Fprintf(buff, "Object: %-30s %s%s\n", v.name, "Total: ", secondsToHuman(v.L*v.Lexpo+v.R*v.Rexpo+v.G*v.Gexpo+v.B*v.Bexpo+v.S*v.Sexpo+v.H*v.Hexpo+v.O*v.Oexpo))
+			if v.L > 0 || v.R > 0 || v.G > 0 || v.B > 0 {
+				fmt.Fprintln(buff)
+			}
 			if v.L > 0 {
 				fmt.Fprintf(buff, "L\tNb: %4d\tExpo: %4ds\tSubs: %s\n", v.L, v.Lexpo, secondsToHuman(v.L*v.Lexpo))
 			}
@@ -226,6 +273,21 @@ func (ts *targets) printObjects(wdest writeDestination) {
 			}
 			if v.B > 0 {
 				fmt.Fprintf(buff, "B\tNb: %4d\tExpo: %4ds\tSubs: %s\n", v.B, v.Bexpo, secondsToHuman(v.B*v.Bexpo))
+			}
+			if v.R1 > 0 || v.G1 > 0 || v.B1 > 0 {
+				fmt.Fprintln(buff)
+			}
+			if v.R1 > 0 {
+				fmt.Fprintf(buff, "R\tNb: %4d\tExpo: %4ds\tSubs: %s\n", v.R1, v.R1expo, secondsToHuman(v.R1*v.R1expo))
+			}
+			if v.G1 > 0 {
+				fmt.Fprintf(buff, "G\tNb: %4d\tExpo: %4ds\tSubs: %s\n", v.G1, v.G1expo, secondsToHuman(v.G1*v.G1expo))
+			}
+			if v.B1 > 0 {
+				fmt.Fprintf(buff, "B\tNb: %4d\tExpo: %4ds\tSubs: %s\n", v.B1, v.B1expo, secondsToHuman(v.B1*v.B1expo))
+			}
+			if v.S > 0 || v.H > 0 || v.O > 0 {
+				fmt.Fprintln(buff)
 			}
 			if v.S > 0 {
 				fmt.Fprintf(buff, "S\tNb: %4d\tExpo: %4ds\tSubs: %s\n", v.S, v.Sexpo, secondsToHuman(v.S*v.Sexpo))
